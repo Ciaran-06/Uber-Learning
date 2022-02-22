7904e929b443dabfe26f17dbb3be20e166bc6407
Ciaran-06 <76187734+Ciaran-06@users.noreply.github.com>
2022-02-22 16:17:17 +0000

    working on options ui

--- src/App.js
+++ src/App.js
@@ -4,12 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          test
-        </p>
-      </header>
+      <div className="App-Options">
+        </div>
     </div>
   );
 }
