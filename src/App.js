22466c67ac5a841d56a6233c6bbe38e139197289
Ciaran-06 <76187734+Ciaran-06@users.noreply.github.com>
2022-02-22 16:55:54 +0000

    added menu title and burger menu

--- src/App.js
+++ src/App.js
@@ -5,9 +5,18 @@ function App() {
   return (
     <div className="App">
       <div className="App-Options">
+        <div className="Ui-Head">
+          <h1 id="Head-Text">Uber Learning</h1>
+          <div className="Ui-Menu">
+            <div className="bar1"></div>
+            <div className="bar2"></div>
+            <div className="bar3"></div>
+            </div>
+          </div>
         </div>
     </div>
   );
 }
+
 
 export default App;
